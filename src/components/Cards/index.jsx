b1f248fcf312640e5f0d8fbf4c77f9c765ebe360
Lucas Luna <112989163+LuccasLuna@users.noreply.github.com>
2024-03-20 21:58:52 -0300

    Add files via upload

--- src/components/Cards/index.jsx
+++ src/components/Cards/index.jsx
@@ -5,15 +5,14 @@ import background from '../../assets/img/ondas-verde2.svg';
 import { Buttons } from '../Buttons'
 
 export class Cards extends Component{
-
-
   render() {
-    const { decks, deckId } = this.props;
+    // const { decks, deckId } = this.props;
     return(
       <div className='main-container' style={{backgroundImage: `url(${background})`}}>
         <div className="container-cards">
           <div className='cards'>
             <div className='card' style={{backgroundColor: '#fff'}}>PERGUNTA</div>
+            {/* essa div de resposta deveria existir? achuquenaum */}
             <div className='card' style={{backgroundColor: '#15AA73', display: 'none'} }>ESTE CARD SÓ SERA CHAMADO QUANDO APERTAR O BOTAO FLIP</div> 
           </div>
           <div className='buttons'>
